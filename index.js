7666f472ca1b4cb419b67a4b0e0603464249b25b
harmanpreet ghotra <harmanpreetghotra17@gmail.com>
2023-06-08 15:21:26 -0400

    new functions added

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+let employee = {
+    "name": "Sam" ,
+    "streetAddress": "11 Broadway"
+}
+
+
+function updateEmployeeWithKeyAndValue(employee,Object,key) { 
+    const newObj = { ...employee, 
+    ["name"]: "Sam",
+    ["streetAddress"]: "11 Broadway"
+    }
+return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee) {
+    employee.name = "Sam" ,
+    employee.streetAddress = "12 Broadway"
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const clone = Object.assign({}, employee);
+    delete clone["name"],
+    ["streetAddress"]
+    return clone;
+  }
+  
+  function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee ["name"],
+    ["streetAddress"]
+  return employee
+  }
+  
+  
